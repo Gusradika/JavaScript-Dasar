2c00e10aff12a3a328c6a30888f467d288cec987
Aditya Kesuma <aditezo24@gmail.com>
2022-08-05 21:35:10 +0700

    ControlFlow and Loops

---
+++ Latihan4-Control Flow Loop/script.js
@@ -0,0 +1,19 @@
+alert("ControlFlow and Loops");
+
+
+for(var i=0; i<5; i++){
+    alert("Looping For : " + (i+1));
+}
+
+var j = 0;
+while(j < 5){
+    alert("Looping While : " + (j+1));
+    j++;
+}
+
+var k = 0;
+
+do{
+    alert("Looping do While : " + (k+1));
+    k++;
+}while(k < 5);
