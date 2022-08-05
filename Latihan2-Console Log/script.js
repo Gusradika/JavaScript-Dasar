e715a237ad81c98e28788f14b9af2b924c47ac7f
Aditya Kesuma <aditezo24@gmail.com>
2022-08-05 20:14:36 +0700

    Console Log

---
+++ Latihan2-Console Log/script.js
@@ -0,0 +1,9 @@
+var x = 10;
+var y = 15;
+var z = 0;
+
+
+console.log("Hellow World \n nilai x adalah " + x);
+console.log("Nilai y adalah " + y + "\n");
+z = x + y;
+console.log(x + " + " + y + " = " + z);
